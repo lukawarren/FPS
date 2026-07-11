b8e43ac8d403ebb1a25f7436c480257002689abf
Luka Warren <lukawarren1@gmail.com>
2026-07-11 10:07:49 +0100

    Restored render scale

--- include/common.h
+++ include/common.h
@@ -82,7 +82,7 @@ inline std::string AUDIO_ROOT = "../res/audio/";
 
 struct QualitySettings
 {
-    u32 inverse_render_scale = 1;
+    u32 inverse_render_scale = 3;
     u32 shadow_map_width = 1024;
     u32 shadow_map_height = 1024;
     u32 bloom_downsamples = 3;
